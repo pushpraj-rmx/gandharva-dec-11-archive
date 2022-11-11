8b9d7c020e18ff91956a0296e09ad786436a9f67
Pushpraj Dwivedi <akshaydwivedi19@gmail.com>
2022-11-11 15:35:41 +0530

    'commit-1'

---
+++ @trash/header.php
@@ -0,0 +1,118 @@
+<header class="section page-header"> 
+      <!-- RD Navbar-->
+      <div class="rd-navbar-wrap">
+        <nav class="rd-navbar rd-navbar-classic rd-navbar-classic-center" data-layout="rd-navbar-fixed" data-sm-layout="rd-navbar-fixed" data-md-layout="rd-navbar-fixed" data-md-device-layout="rd-navbar-fixed" data-lg-layout="rd-navbar-static" data-lg-device-layout="rd-navbar-static" data-xl-layout="rd-navbar-static" data-xl-device-layout="rd-navbar-static" data-lg-stick-up-offset="46px" data-xl-stick-up-offset="46px" data-xxl-stick-up-offset="46px" data-lg-stick-up="true" data-xl-stick-up="true" data-xxl-stick-up="true">
+          <div class="rd-navbar-collapse-toggle rd-navbar-fixed-element-1" data-rd-navbar-toggle=".rd-navbar-collapse"><span></span></div>
+          <div class="rd-navbar-aside-outer rd-navbar-collapse">
+            <div class="rd-navbar-aside">
+              <div class="header-info">
+                <ul class="list-inline list-inline-md">
+                  <li>
+                    <div class="unit unit-spacing-xs align-items-center">
+                      <div class="unit-left font-weight-bold">Call Us:</div>
+                      <div class="unit-body"><a href="tel:#">(073) 123-12-12</a></div>
+                    </div>
+                  </li>
+                  <li>
+                    <div class="unit unit-spacing-xs align-items-center">
+                      <div class="unit-left font-weight-bold">Opening Hours:</div>
+                      <div class="unit-body"> Mn-Fr: 10 am-8 pm</div>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+              <div class="social-block">
+                <ul class="list-inline">
+                  <li><a class="icon fa-facebook" href="#"></a></li>
+                  <li><a class="icon fa-twitter" href="#"></a></li>
+                  <li><a class="icon fa-google-plus" href="#"></a></li>
+                  <li><a class="icon fa-vimeo" href="#"></a></li>
+                  <li><a class="icon fa-youtube" href="#"></a></li>
+                  <li><a class="icon fa-pinterest-p" href="#"></a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="rd-navbar-main-outer">
+            <div class="rd-navbar-main"> 
+              <!-- RD Navbar Panel-->
+              <div class="rd-navbar-panel"> 
+                <!-- RD Navbar Toggle-->
+                <button class="rd-navbar-toggle" data-rd-navbar-toggle=".rd-navbar-nav-wrap"><span></span></button>
+                <!-- RD Navbar Brand-->
+                <div class="rd-navbar-brand"><a href="index.html"><img src="images/gandharva_logo.png" alt="" /></a></div>
+              </div>
+              <div class="rd-navbar-main-element">
+                <div class="rd-navbar-nav-wrap">
+                  <ul class="rd-navbar-nav">
+                    <li class="rd-nav-item active"><a class="rd-nav-link" href="index.html">Home</a></li>
+                    <li class="rd-nav-item"><a class="rd-nav-link" href="#">About Us</a>
+                      <ul class="rd-menu rd-navbar-dropdown">
+                        <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="">Our Team</a></li>
+                        <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="">Our Philosophy</a></li>
+                        <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="">Our Partners</a></li>
+                        <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="">About the Architect</a></li>
+                        <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="">Sustainability</a></li>
+                        <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="">Gallery</a></li>
+                      </ul>
+                    </li>
+                    <li class="rd-nav-item"><a class="rd-nav-link" href="#">Experiences</a>
+                      <ul class="rd-menu rd-navbar-dropdown">
+                        <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="">All Experiences</a></li>
+                        <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="">Fitness & Recreation</a></li>
+                        <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="">Countryside Getaway</a></li>
+                        <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="">Signature Dining</a></li>
+                        <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="">Art at Dwaar</a></li>
+                        
+                      </ul>
+                    </li>
+                    <li class="rd-nav-item"><a class="rd-nav-link" href="#">Programmes</a>
+                      <ul class="rd-menu rd-navbar-dropdown">
+                        <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="">All Treatments</a></li>
+                        <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="">Panchakarma</a></li>
+                        <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="">Weight Management</a></li>
+                        <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="">Vedic Acupressure Kayakalpa Treatment</a></li>
+                        <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="">Vedic Healing</a></li>
+                        <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="">Pain Management</a></li>
+                        <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="">Weekend Detox</a></li>
+                        <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="">Vedic Acupressure Full Body Detox</a></li>
+                        <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="">Vedic Acupressure Shuddhi Treatment</a></li>
+                        <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="">Revive package</a></li>
+                      </ul>
+                    </li>
+                    
+                    <li class="rd-nav-brand logos"><a href="index.html"><img src="images/gandharva_logo.png" alt="" /></a></li>
+                    <li class="rd-nav-item"><a class="rd-nav-link" href="#">Events </a> 
+                      <!-- RD Navbar Dropdown-->
+                      <ul class="rd-menu rd-navbar-dropdown">
+                        <li class="rd-dropdown-item"><a class="rd-dropdown-link" href=""> All Events</a></li>
+                        <li class="rd-dropdown-item"><a class="rd-dropdown-link" href=""> Personal</a></li>
+                        <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="">Corporate</a></li>
+                       
+                      </ul>
+                    </li>
+                    <li class="rd-nav-item"><a class="rd-nav-link" href="#">Accommodation </a> 
+                      <!-- RD Navbar Dropdown-->
+                      <ul class="rd-menu rd-navbar-dropdown">
+                        <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="">All Rooms</a></li>
+                        <li class="rd-dropdown-item"><a class="rd-dropdown-link" href=""> Suite</a></li>
+                        <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="">Premium Room</a></li>
+                        <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="">Herbal Garden Room</a></li>
+                        <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="">Grand Garden Room</a></li>
+                        <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="">Ayurvedic Garden Room</a></li>
+                        <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="">Ayurvedic Suite</a></li>
+                        <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="">Gher Villa</a></li>
+                        
+                      </ul>
+                    </li>
+                   
+                   <li class="rd-nav-item"><a class="rd-nav-link" href="contacts.html">Contacts</a></li>
+                 
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </header>
